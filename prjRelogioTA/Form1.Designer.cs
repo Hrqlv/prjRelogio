dcf0231d904c300248f595cd6d8fedf0f9776276
Henrique Lopes Velozo <henriquelopesvelozo@gmail.com>
2022-09-19 22:26:11 -0300

    relogio

---
+++ prjRelogioTA/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace prjRelogioTA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbRelogio = new System.Windows.Forms.PictureBox();
+            this.relogio = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnPonteiro = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.mnFundo = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRelogio)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbRelogio
+            // 
+            this.pbRelogio.BackColor = System.Drawing.Color.White;
+            this.pbRelogio.Location = new System.Drawing.Point(3, 27);
+            this.pbRelogio.Name = "pbRelogio";
+            this.pbRelogio.Size = new System.Drawing.Size(326, 287);
+            this.pbRelogio.TabIndex = 0;
+            this.pbRelogio.TabStop = false;
+            // 
+            // relogio
+            // 
+            this.relogio.Enabled = true;
+            this.relogio.Interval = 1000;
+            this.relogio.Tick += new System.EventHandler(this.relogio_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnPonteiro,
+            this.mnFundo});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(329, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Tag = "";
+            this.menuStrip1.Text = "menu";
+            // 
+            // mnPonteiro
+            // 
+            this.mnPonteiro.Name = "mnPonteiro";
+            this.mnPonteiro.Size = new System.Drawing.Size(64, 20);
+            this.mnPonteiro.Text = "Ponteiro";
+            this.mnPonteiro.Click += new System.EventHandler(this.mnPonteiro_Click);
+            // 
+            // mnFundo
+            // 
+            this.mnFundo.Name = "mnFundo";
+            this.mnFundo.Size = new System.Drawing.Size(53, 20);
+            this.mnFundo.Text = "Fundo";
+            this.mnFundo.Click += new System.EventHandler(this.mnFundo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Imagens*.BMP;*JPG;*.PNG";
+            this.openFileDialog1.FilterIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(329, 334);
+            this.Controls.Add(this.pbRelogio);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Relogio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRelogio)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbRelogio;
+        private System.Windows.Forms.Timer relogio;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnPonteiro;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ToolStripMenuItem mnFundo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ prjRelogioTA/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjRelogioTA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string caminho = Environment.CurrentDirectory + "\\fundo.png";
+        Graphics g; //area grafica do PictureBox
+        Bitmap desenho; //desenho do relogio
+        Color corPonteiro = Color.Black;
+
+        int hora = 0;
+        int min = 0;
+        int seg = 0;
+
+        int xseg, yseg; // ponteiro do segundo
+        int xmin, ymin; // ponteiro do minuto
+        int xhora, yhora; // ponteiro da hora
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            desenho = new Bitmap(pbRelogio.Width,
+                pbRelogio.Height);
+            g = Graphics.FromImage(desenho);
+
+            if (!File.Exists(caminho))
+            {
+                relogio.Enabled = false;
+                mnFundo_Click(sender, e);
+                relogio.Enabled = true;
+            }
+        }
+
+        private void relogio_Tick(object sender, EventArgs e)
+        {
+            DateTime agora = DateTime.Now;
+            hora = agora.Hour;
+            min = agora.Minute;
+            seg = agora.Second;
+            DesenharRelogio();
+            DesenharPonteiroSegundo();
+            DesenharPonteiroMinuto();
+            DesenharPonteiroHora();
+            DesenharCentro();
+            DesenharDigital();
+            pbRelogio.CreateGraphics().DrawImage(
+                desenho, 0, 0);
+            GC.Collect(); // chamar a lixeira
+        }
+
+        private void DesenharDigital()
+        {
+            SolidBrush corSolida = new SolidBrush(Color.Red);
+            g.DrawString(
+                DateTime.Now.ToLongTimeString(),
+                new Font("Arial",12),
+                corSolida, 100, 100);
+        }
+
+        private void DesenharRelogio()
+        {
+            g.DrawImage(Image.FromFile(caminho), 0, 0,
+                pbRelogio.Width, pbRelogio.Height);
+        }
+
+        private void DesenharCentro()
+        {
+            SolidBrush corSolida = new SolidBrush(Color.SaddleBrown);
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            g.FillEllipse(corSolida, cx - 10, cy - 10, 16, 16);
+        }
+
+        private void DesenharPonteiroHora()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 70;
+            if (hora > 12) hora = hora - 12;
+            double angulo = -90 + (hora * 30);
+            Pen caneta = new Pen(Color.White, 8);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            g.DrawLine(caneta, cx, cy, cx + xhora, cy + yhora);
+            double rad = Math.PI * angulo / 180;
+            xhora = (int)(raio * Math.Cos(rad));
+            yhora = (int)(raio * Math.Sin(rad));
+            caneta.Color = corPonteiro;
+            g.DrawLine(caneta, cx, cy, cx + xhora, cy + yhora);
+
+        }
+
+        private void DesenharPonteiroMinuto()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 110;
+            double angulo = -90 + (min * 6);
+            angulo += (min / 12) * 6;
+            Pen caneta = new Pen(Color.White, 5);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            g.DrawLine(caneta, cx, cy, cx + xmin, cy + ymin);
+            double rad = Math.PI * angulo / 180;
+            xmin = (int)(raio * Math.Cos(rad));
+            ymin = (int)(raio * Math.Sin(rad));
+            caneta.Color = corPonteiro;
+            g.DrawLine(caneta, cx, cy, cx + xmin, cy + ymin);
+        }
+
+        private void DesenharPonteiroSegundo()
+        {
+            int cx = pbRelogio.Width / 2;
+            int cy = pbRelogio.Height / 2;
+            int raio = 90;
+            double angulo = -90 + (seg * 6);
+            Pen caneta = new Pen(Color.Transparent, 5);
+            caneta.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            g.DrawLine(caneta, cx, cy, cx + xseg, cy + yseg);
+            double rad = Math.PI * angulo / 180;
+            xseg = (int)(raio * Math.Cos(rad));
+            yseg = (int)(raio * Math.Sin(rad));
+            caneta.Color = corPonteiro;
+            g.DrawLine(caneta, cx, cy, cx + xseg, cy + yseg);
+        }
+
+        private void mnPonteiro_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            corPonteiro = colorDialog1.Color;
+        }
+
+        private void mnFundo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            relogio.Stop();
+            File.Delete(caminho);
+            File.Copy(openFileDialog1.FileName,
+                caminho);
+            relogio.Start();
+        }
+    }
+}
